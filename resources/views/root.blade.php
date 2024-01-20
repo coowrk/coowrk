d2f9337510b1eef6459aa8a7b9d1fa208de4b240
VinAdel <vinadel+github@outlook.de>
2024-01-20 18:55:50 +0100

    chore: add livewire styles and scripts

---
+++ resources/views/root.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    {{-- Meta data --}}
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- Title --}}
+    <title>{{ App::config('app.name') }}</title>
+
+    {{-- Livewire Styles --}}
+    @livewireStyles
+</head>
+
+<body class="antialiased">
+
+    {{-- Livewire Scripts --}}
+    @livewireScripts
+</body>
+
+</html>
