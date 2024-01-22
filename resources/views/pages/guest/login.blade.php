313197b7d33095aa006221c8e9b513ca8ef7c877
VinAdel <vinadel+github@outlook.de>
2024-01-22 23:55:38 +0100

    feat: :lipstick: edit login and form components

--- resources/views/pages/guest/login.blade.php
+++ resources/views/pages/guest/login.blade.php
@@ -34,10 +34,9 @@
                     </div>
 
                     <div>
-                        <button type="submit"
-                            class="flex w-full justify-center rounded-md bg-app-yellow px-3 py-1.5 text-sm font-semibold leading-6 text-zinc-950 shadow-sm hover:bg-app-yellow/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-app-yellow">
+                        <x-forms.button type="submit">
                             Anmelden
-                        </button>
+                        </x-forms.button>
                     </div>
                 </div>
             </form>
