a3da67ed5239170878ab4134535ad331ba498414
VinAdel <vinadel+github@outlook.de>
2024-07-29 23:25:39 +0200

    chore: root changes

---
+++ resources/views/components/root.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+{{-- html --}}
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+	{{-- head --}}
+
+	<head>
+		{{-- meta tags --}}
+		<meta charset="utf-8">
+		<meta
+			content="width=device-width, initial-scale=1"
+			name="viewport"
+		>
+		<meta
+			content="ie=edge"
+			http-equiv="X-UA-Compatible"
+		>
+
+		{{-- title --}}
+		<title>{{ config('app.name') }}</title>
+
+		{{-- styles --}}
+		@livewireStyles
+		@vite('resources/styles/app.css')
+	</head>
+
+	{{-- body --}}
+
+	<body>
+		{{-- content --}}
+		{{ $slot }}
+
+		{{-- scripts --}}
+		@livewireScripts
+	</body>
+
+</html>
