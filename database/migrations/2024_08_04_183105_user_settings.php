edd1151cd66cd2523f9dbf0c1853aa31b543da53
VinAdel <vinadel+github@outlook.de>
2024-08-04 20:33:47 +0200

    chore: user settings migration

---
+++ database/migrations/2024_08_04_183105_user_settings.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_settings', function (Blueprint $table) {
+            $table->id();
+            $table->enum('theme', [0, 1])->default(0);
+            $table->json('misc')->nullable()->comment('e. g. last visited page of pagination settings etc.');
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
