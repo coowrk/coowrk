1ad17027b98e7a809dfcab89128ab7a93740bcc8
VinAdel <vinadel+github@outlook.de>
2024-08-08 15:49:35 +0200

    chore: add comment icon on short-letter feed

--- resources/views/components/feeds/item/text.blade.php
+++ resources/views/components/feeds/item/text.blade.php
@@ -1,3 +1,3 @@
-<div class="mt-2 text-base/6 text-zinc-500 dark:text-zinc-400 sm:text-sm/6">
+<div class="text-base/6 text-zinc-500 dark:text-zinc-400 sm:text-sm/6">
 	{{ $slot }}
 </div>
--- resources/views/components/feeds/status.blade.php
+++ resources/views/components/feeds/status.blade.php
@@ -1,18 +1,20 @@
 {{-- feed status --}}
 <x-feeds.item.base :date="$data->created_at->diffForHumans()">
+	{{-- text --}}
+	<x-feeds.item.text>
+		{{ $data->created_by->name }}
+	</x-feeds.item.text>
+
 	<div class="flex flex-wrap gap-x-2 gap-y-1">
 		{{-- title --}}
 		<x-feeds.item.title>{{ __($data->title) }}</x-feeds.item.title>
 
 		{{-- badge --}}
-		<x-badges
-			text="{{ __($badge['title']) }}"
-			theme="{{ $badge['theme'] }}"
-		/>
+		@if ($data->new_status)
+			<x-badges
+				text="{{ __($badge['title']) }}"
+				theme="{{ $badge['theme'] }}"
+			/>
+		@endif
 	</div>
-
-	{{-- text --}}
-	<x-feeds.item.text>
-		{{ $data->created_by->name }}
-	</x-feeds.item.text>
 </x-feeds.item.base>
