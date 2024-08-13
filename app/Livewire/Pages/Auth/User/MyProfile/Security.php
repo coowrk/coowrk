9304e97d7145f7fca9ab9c6ea862f02df3845e97
VinAdel <vinadel+github@outlook.de>
2024-08-13 14:13:36 +0200

    chore: add sessions to my-profile security

--- app/Livewire/Pages/Auth/User/MyProfile/Security.php
+++ app/Livewire/Pages/Auth/User/MyProfile/Security.php
@@ -7,10 +7,18 @@ use Livewire\Component;
 
 class Security extends Component
 {
+    // properties
+    public $sessions = [];
+
     // render html
     #[Layout('components.layouts.pages.auth.user.my-profile.default')]
     public function render()
     {
         return view('pages.auth.user.my-profile.security');
+    }
+
+    public function mount()
+    {
+        $this->sessions = auth()->user()->sessions;
     }
 }
--- resources/views/pages/auth/user/my-profile/security.blade.php
+++ resources/views/pages/auth/user/my-profile/security.blade.php
@@ -1,3 +1,25 @@
 <div>
+	<x-typography.title.h1 title="Sicherheit" />
 
+	<div class="mt-6">
+		<div class="flex items-end justify-between">
+			<x-typography.title.h3 title="Passwort" />
+
+			<x-forms.buttons theme="white">
+				Passwort ändern
+			</x-forms.buttons>
+		</div>
+		<x-typography.divider class="my-4" />
+
+		<x-typography.text text="Stärke dein Konto, indem Du sicherstellst, dass dein Passwort sicher ist." />
+	</div>
+
+	<div class="mt-6">
+		<x-typography.title.h3 title="Verbindungen" />
+		<x-typography.text text="Alle Sessions vom User" />
+		<x-typography.divider class="my-4" />
+
+		@foreach ($sessions as $session)
+		@endforeach
+	</div>
 </div>
