e180f9a8bd9098c505e1f8131be305188ceabf6d
VinAdel <vinadel+github@outlook.de>
2024-08-15 23:33:43 +0200

    chore: add user change name to my-profile general

--- resources/views/pages/auth/user/my-profile/general.blade.php
+++ resources/views/pages/auth/user/my-profile/general.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div x-data="{ showInputName: @entangle('showInputName'), showInputEmail: @entangle('showInputEmail') }">
 	<x-typography.title.h1 title="Mein Profil" />
 
 	<div class="mt-6">
@@ -9,12 +9,65 @@
 		<x-descriptionlists.base>
 			<x-descriptionlists.term text="Name" />
 			<x-descriptionlists.detail>
-				{{ auth()->user()->first_name }}
-				{{ auth()->user()->last_name }}
+				<div
+					class="flex items-start justify-between"
+					x-show="!showInputName"
+				>
+					<div>
+						{{ auth()->user()->first_name }}
+						{{ auth()->user()->last_name }}
+					</div>
+
+					<button
+						class="font-medium text-blue-500"
+						x-on:click="showInputName = true"
+					>
+						Bearbeiten
+					</button>
+				</div>
+
+				<form
+					wire:submit="updateUserName"
+					x-show="showInputName"
+				>
+					<div class="flex items-start justify-between gap-4">
+						<div class="w-full">
+							<x-forms.input
+								for="first_name"
+								title="first_name"
+							/>
+						</div>
+
+						<div class="w-full">
+							<x-forms.input
+								for="last_name"
+								title="last_name"
+							/>
+						</div>
+					</div>
+
+					<div class="mt-4 flex justify-end">
+						<x-forms.buttons theme="white">
+							Bearbeiten
+						</x-forms.buttons>
+					</div>
+				</form>
 			</x-descriptionlists.detail>
 
 			<x-descriptionlists.term text="E-Mail Adresse" />
-			<x-descriptionlists.detail :text="auth()->user()->email" />
+			<x-descriptionlists.detail>
+				{{-- <div class="flex justify-between">
+					<div>
+						{{ auth()->user()->email }}
+					</div>
+
+					<button class="font-medium text-blue-500">
+						Bearbeiten
+					</button>
+				</div> --}}
+
+				{{ auth()->user()->email }}
+			</x-descriptionlists.detail>
 
 			<x-descriptionlists.term text="Mitglied seit" />
 			<x-descriptionlists.detail :text="auth()->user()->created_at" />
