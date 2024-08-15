305c31120b0bcedc8b1d6bd0cbf850c07ab061db
VinAdel <vinadel+github@outlook.de>
2024-08-16 00:40:32 +0200

    chore: add cancellation index

--- app/Livewire/Pages/Auth/Cancellation/Index.php
+++ app/Livewire/Pages/Auth/Cancellation/Index.php
@@ -2,10 +2,13 @@
 
 namespace App\Livewire\Pages\Auth\Cancellation;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    // render html
+    #[Layout('components.layouts.pages.auth.default')]
     public function render()
     {
         return view('pages.auth.cancellation.index');
