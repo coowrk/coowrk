b68ea60e888d181b471747e299c91eed90bb2b4f
VinAdel <vinadel+github@outlook.de>
2024-08-15 23:39:05 +0200

    chore: remove update user name on my-profile general

--- app/Livewire/Pages/Auth/User/MyProfile/General.php
+++ app/Livewire/Pages/Auth/User/MyProfile/General.php
@@ -2,50 +2,15 @@
 
 namespace App\Livewire\Pages\Auth\User\MyProfile;
 
-use Illuminate\Support\Arr;
 use Livewire\Attributes\Layout;
-use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class General extends Component
 {
-    // properties
-    public $showInputName = false;
-    // public $showInputEmail = false;
-
-    #[Validate(['required', 'min:2', 'max:255'])]
-    public $first_name;
-
-    #[Validate(['required', 'min:2', 'max:255'])]
-    public $last_name;
-
-    // public $email;
-
     // render html
     #[Layout('components.layouts.pages.auth.user.my-profile.default')]
     public function render()
     {
         return view('pages.auth.user.my-profile.general');
-    }
-
-    // fetch data
-    public function mount()
-    {
-        $this->first_name = auth()->user()->first_name;
-        $this->last_name = auth()->user()->last_name;
-        // $this->email = auth()->user()->email;
-    }
-
-    // update name
-    public function updateUserName()
-    {
-        // update user name
-        // validate input data
-        auth()->user()->update(
-            Arr::only($this->validate(), ['first_name', 'last_name'])
-        );
-
-        // hide name inputs
-        $this->showInputName = false;
     }
 }
--- resources/views/pages/auth/user/my-profile/general.blade.php
+++ resources/views/pages/auth/user/my-profile/general.blade.php
@@ -9,65 +9,12 @@
 		<x-descriptionlists.base>
 			<x-descriptionlists.term text="Name" />
 			<x-descriptionlists.detail>
-				<div
-					class="flex items-start justify-between"
-					x-show="!showInputName"
-				>
-					<div>
-						{{ auth()->user()->first_name }}
-						{{ auth()->user()->last_name }}
-					</div>
-
-					<button
-						class="font-medium text-blue-500"
-						x-on:click="showInputName = true"
-					>
-						Bearbeiten
-					</button>
-				</div>
-
-				<form
-					wire:submit="updateUserName"
-					x-show="showInputName"
-				>
-					<div class="flex items-start justify-between gap-4">
-						<div class="w-full">
-							<x-forms.input
-								for="first_name"
-								title="first_name"
-							/>
-						</div>
-
-						<div class="w-full">
-							<x-forms.input
-								for="last_name"
-								title="last_name"
-							/>
-						</div>
-					</div>
-
-					<div class="mt-4 flex justify-end">
-						<x-forms.buttons theme="white">
-							Bearbeiten
-						</x-forms.buttons>
-					</div>
-				</form>
+				{{ auth()->user()->first_name }}
+				{{ auth()->user()->last_name }}
 			</x-descriptionlists.detail>
 
 			<x-descriptionlists.term text="E-Mail Adresse" />
-			<x-descriptionlists.detail>
-				{{-- <div class="flex justify-between">
-					<div>
-						{{ auth()->user()->email }}
-					</div>
-
-					<button class="font-medium text-blue-500">
-						Bearbeiten
-					</button>
-				</div> --}}
-
-				{{ auth()->user()->email }}
-			</x-descriptionlists.detail>
+			<x-descriptionlists.detail text="{{ auth()->user()->email }}" />
 
 			<x-descriptionlists.term text="Mitglied seit" />
 			<x-descriptionlists.detail :text="auth()->user()->created_at" />
