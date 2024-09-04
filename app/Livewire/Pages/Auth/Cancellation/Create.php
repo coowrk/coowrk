a32a22ebc4abaed5fab6ca7a477d554ded411239
VinAdel <vinadel+github@outlook.de>
2024-09-04 22:16:21 +0200

    chore: edit cancellation create ressource

--- app/Livewire/Pages/Auth/Cancellation/Create.php
+++ app/Livewire/Pages/Auth/Cancellation/Create.php
@@ -3,10 +3,36 @@
 namespace App\Livewire\Pages\Auth\Cancellation;
 
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Create extends Component
 {
+    // properties
+    #[Validate(['required', 'in:Herr,Frau,Divers,Firma'])]
+    public $salutation;
+
+    #[Validate(['required', 'min:2', 'max:255'])]
+    public $first_name;
+
+    #[Validate(['max:255', 'required_if:salutation,Herr,Frau,Divers'])]
+    public $last_name;
+
+    #[Validate(['required', 'min:1', 'max:255'])]
+    public $street;
+
+    #[Validate(['required', 'min:1', 'max:255'])]
+    public $house_number;
+
+    #[Validate(['required', 'min:1', 'max:255'])]
+    public $postcode;
+
+    #[Validate(['required', 'min:1', 'max:255'])]
+    public $city;
+
+    #[Validate(['required', 'min:1', 'max:255'])]
+    public $country;
+
     // render html
     #[Layout('components.layouts.pages.auth.default')]
     public function render()
--- resources/views/pages/auth/cancellation/create.blade.php
+++ resources/views/pages/auth/cancellation/create.blade.php
@@ -28,4 +28,164 @@
 	<div class="mt-4 lg:mt-8">
 		<x-typography.title.h1 title="Erstellen" />
 	</div>
+
+	{{-- formular --}}
+	<div class="mt-12">
+		<form
+			class="flex flex-col gap-y-4"
+			wire:submit="create"
+		>
+			{{-- search customer --}}
+			<div class="shrink-0">
+				<div class="flex flex-col">
+					<x-forms.label title="Suche" />
+					<div class="mt-2">
+						<x-forms.buttons.zinc
+							type="button"
+							wire:click="$dispatch('change.short-letter.search-customer-palett.visibility.state')"
+						>
+							Empfänger ermitteln
+						</x-forms.buttons.zinc>
+					</div>
+				</div>
+				<x-typography.divider class="my-8 sm:hidden" />
+			</div>
+
+			{{-- customer options --}}
+			<div class="grid grow grid-cols-1 gap-4 sm:grid-cols-8">
+				<div class="col-span-1 sm:col-span-2">
+					<x-forms.label
+						for="salutation"
+						title="Anrede"
+					/>
+					<x-forms.select
+						:options="[
+						    'Herr' => 'Herr',
+						    'Frau' => 'Frau',
+						    'Divers' => 'Divers',
+						    'Firma' => 'Firma',
+						]"
+						for="salutation"
+						title="first name"
+						wire:model.live="salutation"
+					>
+					</x-forms.select>
+				</div>
+
+				@if ($salutation == 'Firma')
+					<div class="col-span-1 sm:col-span-6">
+						<x-forms.label
+							for="first_name"
+							title="Name"
+						/>
+						<x-forms.input
+							for="first_name"
+							title="first name"
+						/>
+					</div>
+				@else
+					<div class="col-span-1 sm:col-span-3">
+						<x-forms.label
+							for="first_name"
+							title="Vorname"
+						/>
+						<x-forms.input
+							for="first_name"
+							title="first name"
+						/>
+					</div>
+
+					<div class="col-span-1 sm:col-span-3">
+						<x-forms.label
+							for="last_name"
+							title="Nachname"
+						/>
+						<x-forms.input
+							for="last_name"
+							title="last name"
+						/>
+					</div>
+				@endif
+			</div>
+
+			<div class="grid grow grid-cols-1 gap-4 sm:grid-cols-5">
+				<div class="col-span-1 sm:col-span-4">
+					<x-forms.label
+						for="street"
+						title="Straße"
+					/>
+					<x-forms.input
+						for="street"
+						title="street"
+					/>
+				</div>
+
+				<div class="col-span-1">
+					<x-forms.label
+						for="house_number"
+						title="Hausnummer"
+					/>
+					<x-forms.input
+						for="house_number"
+						title="Hausnummer"
+					/>
+				</div>
+			</div>
+
+			<div class="grid grow grid-cols-1 gap-4 sm:grid-cols-5">
+				<div class="col-span-1">
+					<x-forms.label
+						for="postcode"
+						title="Postleitzahl"
+					/>
+					<x-forms.input
+						for="postcode"
+						title="Postleitzahl"
+					/>
+				</div>
+
+				<div class="col-span-1 sm:col-span-3">
+					<x-forms.label
+						for="city"
+						title="Stadt"
+					/>
+					<x-forms.input
+						for="city"
+						title="Stadt"
+					/>
+				</div>
+
+				<div class="col-span-1">
+					<div>
+						<x-forms.label
+							for="country"
+							title="Land"
+						/>
+						<x-forms.input
+							for="country"
+							title="Land"
+						/>
+					</div>
+				</div>
+			</div>
+
+			<div class="mt-4 flex justify-end">
+				<x-forms.buttons.white>
+					<svg
+						class="size-5 sm:size-4 text-zinc-500"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z"
+						/>
+					</svg>
+
+
+					Erstellen
+				</x-forms.buttons.white>
+			</div>
+		</form>
+	</div>
 </div>
