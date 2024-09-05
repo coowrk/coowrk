3daf5f5135e1bfbd7374ee6b11bd032226d2f509
VinAdel <vinadel+github@outlook.de>
2024-09-05 23:08:47 +0200

    chore: add factory for company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'street',
+        'house_number',
+        'postalcode',
+        'city',
+        'country'
+    ];
 }
