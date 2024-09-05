8ccf842f8ccb378c91c0a7a17663c1eafa46700e
VinAdel <vinadel+github@outlook.de>
2024-09-05 22:57:03 +0200

    chore: edit cancellation ressources

--- resources/views/pages/auth/cancellation/create.blade.php
+++ resources/views/pages/auth/cancellation/create.blade.php
@@ -23,6 +23,7 @@
 					fill-rule="evenodd"
 				></path>
 			</svg>
+
 			Kündigung
 		</a>
 	</div>
@@ -194,7 +195,6 @@
 							d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z"
 						/>
 					</svg>
-
 
 					Erstellen
 				</x-forms.buttons.white>
