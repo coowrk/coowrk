53b76c524e052db1e6a91a98c8a4fa6e5d167125
VinAdel <vinadel+github@outlook.de>
2024-09-09 18:47:20 +0200

    fix: first name @ home

--- resources/views/pages/auth/home.blade.php
+++ resources/views/pages/auth/home.blade.php
@@ -1,4 +1,4 @@
 <div>
 	<h2 class="font-medium text-zinc-400">Willkommen zurück, </h2>
-	<h1 class="mt-2 text-3xl font-bold text-white">{{ auth()->user()->name }}</h1>
+	<h1 class="mt-2 text-3xl font-bold text-white">{{ auth()->user()->first_name }}</h1>
 </div>
