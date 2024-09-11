1b2ba54133d68ead618ddf303b67821b694185e3
VinAdel <vinadel+github@outlook.de>
2024-09-11 12:04:42 +0200

    build: clean install of tailwind & base

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+import preset from './vendor/filament/support/tailwind.config.preset'
+ 
+export default {
+    presets: [preset],
+    content: [
+        "./app/Filament/**/*.php",
+        "./resources/views/filament/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        fontFamily: {
+          sans: ["Inter"],
+        },
+      
+        extend: {},
+    },
+}
